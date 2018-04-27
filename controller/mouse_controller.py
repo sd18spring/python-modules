8a964fb33c5866826ba0c39f0c7adb93308db85b
Benjamin Hill <benjamin.hill@olin.edu>
2018-04-27 13:03:14 -0400

    Separated Model, View, and Controller submodules
    Created package subfolders, added __init__.py

---
+++ controller/mouse_controller.py
@@ -0,0 +1,18 @@
+from pygame.locals import *
+import pygame
+
+class Controller:
+    """ Controls Brick Breaker using the mouse """
+    
+    def __init__(self, model):
+        """ Constructs a PyGameMouseController object
+            model: the Brick Breaker game state """
+        self.model = model
+    
+    def handle_pygame_event(self, event):
+        """ handles a PyGame key down event
+            event: a PyGame event of type KEYDOWN """
+        if event.type != MOUSEMOTION:
+            # nothing to do
+            return
+        self.model.paddle.x = event.pos[0]-self.model.paddle.width/2.0
