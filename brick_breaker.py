5298fd48dad4e0547fe6bd8e39c1bfa47c07206d
Benjamin Hill <benjamin.hill@olin.edu>
2018-04-27 12:59:35 -0400

    Separated Controller functionality
    New file with classes for Keyboard and Mouse control

--- brick_breaker.py
+++ brick_breaker.py
@@ -19,42 +19,9 @@ from pygame.locals import *
 # 3) Local modules
 from simple_model import Model
 from pygame_window_view import View
+from controllers import KeyboardController as Controller
+#from controllers import MouseController as Controller
 
-
-class PyGameKeyboardController:
-    """ Controls Brick Breaker using the keyboard """
-    
-    def __init__(self, model):
-        """ Constructs a PyGameKeyboardController object
-            model: the Brick Breaker game state """
-        self.model = model
-    
-    def handle_pygame_event(self, event):
-        """ handles a PyGame key down event
-            event: a PyGame event of type KEYDOWN """
-        if event.type != KEYDOWN:
-            # nothing to do
-            return
-        if event.key == pygame.K_LEFT:
-            self.model.change_paddle_velocity(-1)
-        elif event.key == pygame.K_RIGHT:
-            self.model.change_paddle_velocity(1)
-
-class PyGameMouseController:
-    """ Controls Brick Breaker using the mouse """
-    
-    def __init__(self, model):
-        """ Constructs a PyGameMouseController object
-            model: the Brick Breaker game state """
-        self.model = model
-    
-    def handle_pygame_event(self, event):
-        """ handles a PyGame key down event
-            event: a PyGame event of type KEYDOWN """
-        if event.type != MOUSEMOTION:
-            # nothing to do
-            return
-        self.model.paddle.x = event.pos[0]-self.model.paddle.width/2.0
 
 if __name__ == '__main__':
     pygame.init()
@@ -64,8 +31,7 @@ if __name__ == '__main__':
 
     model = Model(size)
     view = View(screen, model)
-    #controller = PyGameMouseController(model)
-    controller = PyGameKeyboardController(model)
+    controller = Controller(model)
 
     running = True
 
